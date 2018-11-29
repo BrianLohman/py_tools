2e495aeb79f2e98b79ed736f2f2d30ba0ea49e8a
BrianLohman <brian.keith.lohman@gmail.com>
2018-11-29 12:35:16 -0700

    added argparse to batch_chpc.py

--- batch_chpc.py
+++ batch_chpc.py
@@ -1,43 +1,73 @@
-# Build job launchers for quinlan-kp partion of CHPC
+#!/usr/bin/env python
+
+# Build and submit jobs on CHPC
+# STATUS: working: 29 Nov 18
+# USAGE: python batch_chpc.py -n [job name] -c [commands] -w [ntasks/node] -m [mem/task]
+
 import sys
 import os
+import argparse
+import re
 
-# USAGE: python batch_chpc.py [job name] [commands] [ntasks/node] [mem/task]
+parser = argparse.ArgumentParser(description='generate and submit jobs to CHPC based on list of commands')
+parser.add_argument('-n','--job-name', dest='job', help = 'job base name')
+parser.add_argument('-c', '--commands', dest='commands', help = 'list of commands to run, one per line')
+parser.add_argument('-w', '--ntasks-node', dest='ntasks', default = 8, help = 'number of tasks per node')
+parser.add_argument('-m', '--mem', dest='mem', default = 128000, help = 'minimum memory required per node')
+parser.add_argument('-p', '--partition', dest = 'partition', default = "quinlan-shared-kp", help = 'quinlan-kp/quinlan-shared-kp')
+parser.add_argument('-t', '--time', dest='time', default = "12:00:00", type = str, help = 'max run time')
+args = parser.parse_args()
 
-# read in job name
-job = sys.argv[1]
+# count the number of lines in the commands file
+f = open(args.commands)
+contents = f.readlines()
+n_commands = 0
+for line in contents:
+    stripped_line = line.strip()
+    if re.match('^\s*#', stripped_line):
+        continue
+    if not stripped_line:
+	continue
+    else:
+	n_commands += 1
 
-# define commands, the list of tasks to execute
-commands = sys.argv[2]
-
-# number of tasks per node
-ntasks = sys.argv[3]
-
-# memory per task
-mem = sys.argv[4]
-
-# loop though commands, printing toks at the bottom of the standard header
+# loop though commands, printing lines from the commands file at the bottom of the standard header
+# while accounting for tasks per node constraints
 job_fh = None
 line_count = int(1)
 
-for line in open(commands):
+for line in open(args.commands):
     if job_fh == None:
-        job_fh = job + "_" + str(line_count)
+        job_fh = args.job + "_" + str(line_count)
         o = open(job_fh + ".job", "w")
-        o.write('\n'.join(["#!/bin/bash", "#SBATCH --time=12:00:00", "#SBATCH --account=quinlan-kp", "#SBATCH --partition=quinlan-shared-kp", "#SBATCH --nodes=1", "#SBATCH --ntasks="+str(ntasks), "#SBATCH --mem="+str(mem), "#SBATCH --job-name="+job_fh, "#SBATCH -o call-"+job_fh+".out", "#SBATCH -e call-"+job_fh+".err", '\n', str(line)]))
-        line_count += 1
-        continue
+        o.write('\n'.join(["#!/bin/bash", "#SBATCH --time="+args.time, "#SBATCH --account=quinlan-kp", "#SBATCH --partition="+args.partition, "#SBATCH --nodes=1", "#SBATCH --ntasks="+str(args.ntasks), "#SBATCH --mem="+str(args.mem), "#SBATCH --job-name="+job_fh, "#SBATCH -o call-"+job_fh+".out", "#SBATCH -e call-"+job_fh+".err", '\n', str(line)]))
+	# if there is only one command to run
+	if line_count == n_commands:
+            o.close()
+	    os.system("sbatch "+job_fh+".job")
+	    break
 
+	else:
+            line_count += 1
+	    continue
+    
     else:
-	if line_count % int(ntasks) != 0:
+        if line_count == n_commands:
+	    o.write(str(line))
+	    o.close()
+            os.system("sbatch "+job_fh+".job")
+	    break
+	
+        if line_count % int(args.ntasks) != 0:
 	    o.write(str(line))
             line_count += 1
             continue
 
-	if line_count % int(ntasks) == 0: 
+	if line_count % int(args.ntasks) == 0: 
 	    o.write(str(line))
 	    o.close()
             os.system("sbatch "+job_fh+".job")
 	    job_fh = None
 	    line_count += 1
 	    continue
+

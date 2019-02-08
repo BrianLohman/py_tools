b1339f1e374612923f00be7a42559e31d616b024
BrianLohman <brian.keith.lohman@gmail.com>
2019-02-08 14:34:24 -0700

    initial

---
+++ split_bed_by_chrom.py
@@ -0,0 +1,19 @@
+# split a bed file by chrom
+# output a new bed file for each chrom, keeping the input file prefix
+
+import sys
+import pandas
+
+bedfile = sys.argv[1]
+prefix = sys.argv[2]
+
+bed = pandas.read_table(bedfile, header = False)
+bed.columns = ['chrom', 'start', 'end']
+
+# list of chroms
+chroms = bed.chrom.unique()
+
+# iterate over list, subsetting and writing to file
+for chrom in chroms:
+	tmp = bed.loc[bed['chrom'] == chrom ]
+	tmp.to_csv(prefix+'_'+chrom+'.bed', index = False, sep = '\t')

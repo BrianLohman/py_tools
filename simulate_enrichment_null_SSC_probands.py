eb0539e9416b84228daeb8685017109ceee14839
BrianLohman <brian.keith.lohman@gmail.com>
2019-04-11 15:48:40 -0600

    init

---
+++ simulate_enrichment_null_SSC_probands.py
@@ -0,0 +1,101 @@
+# Simulate the expected functional enrichment (GO BP) of the average proband in SSC
+
+import pandas
+import random
+import requests
+from collections import defaultdict
+from collections import Counter
+
+# Read in Simons data
+master = pandas.read_table('15_Jan_19_Simons_master_ancestry_corrected_PRS.txt')
+probands = master.loc[master.phenotype == 2]
+proband_ids = list(probands.simons_id)
+
+# Read in table of variants (med and high impact from GATK)
+variants = pandas.read_table('med_high_GATK_variants.txt')
+
+# Make space to store GO terms during the loop
+all_go_terms = []
+
+for i in range(1,101):
+    #f_out = open('randomized_probands_'+str(i)+'.txt', 'w')
+    sample = random.sample(proband_ids, 413)
+    #print(len(sample))
+
+    # Select randomly sampled probands
+    variants_of_interest = variants.loc[variants.SampleID.isin(sample)]
+
+    # Select genes with at least 1 variant
+    gene_symbols_of_interest = list(set(variants_of_interest.SYMBOL))
+    print(str(len(gene_symbols_of_interest))+' unique genes of interest on iteration '+str(i))
+
+    # Write genes of interest to file
+    with open('./test_genes.txt', 'w') as f:
+        for item in gene_symbols_of_interest:
+            f.write("%s\n" % item)
+
+    # Submit a request to the Panther database for GO enrichment
+    # Assign values to form fields
+    form_data = {'organism':'Homo sapiens',
+           'geneList': ('test_genes.txt', open('./test_genes.txt', 'rb')),
+           'enrichmentType': 'fullGO_process',
+            'correction': 'FDR',
+           'type':'enrichment'}
+
+    # Send request to Panther
+    response = requests.post('http://pantherdb.org/webservices/garuda/tools/enrichment/VER_2/enrichment.jsp?', files = form_data)
+
+    # Convert Panther response(bytes) to pandas data farme
+    text = response.content.decode("utf-8")
+
+    header_keys = ['Id','Name','GeneId','P-value', 'FDR']
+    header2idx = dict(zip(range(len(header_keys)), header_keys))
+
+    result = defaultdict(list)
+
+    for i, line in enumerate(text.split('\n')):
+        if line in ('\r', ''):
+            continue
+
+        for idx, toks in enumerate(line.split('\t')):
+            key = header2idx[idx]
+            result[key].append(toks)
+
+    #print([(k, len(result[k])) for k in result])
+
+    # Convert to pandas data frame
+    df = pandas.DataFrame.from_dict(result)
+    
+    # Remove duplicate column headers
+    df = df.iloc[1:]
+    
+    # convert FDR to float
+    df.FDR = df.FDR.astype(float)
+    
+    #print(df.dtypes)
+
+    # only significant after FDR correction
+    df = df[df['FDR'] < 0.05]
+
+    # Sort by FDR and print to file
+    #df.sort_values(by=['FDR']).to_csv(f_out, index = False, sep = '\t')
+    
+    # list of uniq GO groups
+    uniq_go = list(set(df.Name))
+    all_go_terms.extend(uniq_go)
+    
+# count the number of times each GO term appears in the master list
+uniq_counts = list(Counter(all_go_terms).items())
+
+# convert to dict
+d = {}
+for item in uniq_counts:
+    d[item[0]] = item[1]
+
+# convert to data frame
+d_out = pandas.DataFrame.from_dict(d, orient = 'index')
+d_out.columns = ['count']
+
+# write to file, sorted by count
+d_out.sort_values(by=['count'], ascending = False).to_csv('randomized_probands_GO_BP_counts.txt', sep = '\t', header = False)
+

22869d6eba530c8d195ea38c42b4396aad276706
BrianLohman <brian.keith.lohman@gmail.com>
2019-04-19 14:58:09 -0600

    modified to accept simons format

--- cpg_lookup.py
+++ cpg_lookup.py
@@ -2,7 +2,7 @@
 
 # with bed file, check to see if variants are in possible CpG context
 
-# USAGE: cpg_lookup.py [bed file] [reference genome] 
+# USAGE: cpg_lookup.py [variant file] [reference genome] 
 
 from pyfaidx import Fasta
 import sys
@@ -13,26 +13,32 @@ ref_file = sys.argv[2]
 cpg_bed = open('cpg_only_' + bed_file, 'w')
 fa = Fasta(ref_file, read_ahead = 100000, as_raw = True, sequence_always_upper = True)
 
-# loop over variants in bed file
+# loop over variants in variant file
 k = 0
-for chrom, start, end, ref, alt, sample,  in (line.strip().split('\t') for line in open(bed_file)):
+for snp, sample, batch, allele, consequence, impact, symbol  in (line.strip().split('\t') for line in open(bed_file)):
     k += 1
-    if k % 10000 == 0:
+    if k % 100 == 0:
         print(str(k) + ' variants screened', file = sys.stderr)
+
+    if snp == 'ID':
+        continue
     
+    chrom, end, ref, alt = snp.strip().split(':')
+    start = int(end) - 1
+
     if ref == 'C':
-        assert(ref == fa[chrom][int(start)])
+        assert(ref == fa[chrom][start])
         three_prime_base = fa[chrom][int(end)]
         if three_prime_base == 'G':
-            print(chrom, start, end, ref, alt, sample, sep = '\t', file = cpg_bed)
+            print(snp, sample, batch, allele, consequence, impact, symbol,  sep = '\t', file = cpg_bed)
         else:
             continue
 
     if ref == 'G':
-        assert(ref == fa[chrom][int(start)])
+        assert(ref == fa[chrom][start])
         three_prime_base = fa[chrom][int(start) - 1]
         if three_prime_base == 'C':
-            print(chrom, start, end, ref, alt, sample, sep = '\t', file = cpg_bed)
+            print(snp, sample, batch, allele, consequence, impact, symbol, sep = '\t', file = cpg_bed)
         else:
             continue
     

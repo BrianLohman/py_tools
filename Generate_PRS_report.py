834f6a67fc0a31648a7ed22a7ec0df608e461e1b
BrianLohman <brian.keith.lohman@gmail.com>
2019-06-04 12:35:20 -0600

    tables draw but error on redraw

--- Generate_PRS_report.py
+++ Generate_PRS_report.py
@@ -9,20 +9,22 @@ import pandas as pd
 import plotly.plotly as py
 import plotly.graph_objs as go
 import numpy as np
+import json
+import re
 
 
 # In[2]:
 
 
 # define input and output files
-simulated_qvals_fh = "../topGO_10K_raw_qvals.txt"
-simulated_sample_dict_fh = "../topGO_10K_sample_dict.txt"
-simulated_gene_dict_fh = "../topGO_10K_gene_table.txt"
+simulated_qvals_fh = "test_topGO_10K_raw_qvals.txt"
+simulated_sample_dict_fh = "test_topGO_10K_sample_dict.txt"
+simulated_gene_dict_fh = "test_topGO_10K_gene_table.txt"
 observed_data_fh = '../first_EA_p1_data.txt'
 observed_genes_fh = '../first_EA_p1_gene_list.txt'
 observed_samples_fh = '../first_EA_p1_sample_list.txt'
 
-report = "Q1_EA_P1_report.html"
+report = "test_report.html"
 
 
 # In[3]:
@@ -62,6 +64,9 @@ scatter_df = observed_df.loc[observed_df.q_val <= 0.05]
 # get matching simulated values and reorder to match the observed values
 null_scatter_df = nlog_null_qvals.loc[:, nlog_null_qvals.columns.isin(list(scatter_df.GO_term))]
 null_scatter_df = null_scatter_df[scatter_df.GO_term]
+
+#build a dict of name to GO term
+name_dict = pd.Series(scatter_df.GO_term.values, index = scatter_df.Name).to_dict()
 
 
 # In[4]:
@@ -175,150 +180,184 @@ enrichment_by_size_div = plot(fig, validate = False, include_plotlyjs=False, out
 # In[7]:
 
 
-## plotting histogram of null q-values
-test_go_term = "GO:0086010" # user input goes here
+def make_hist_div(hist_vals, hist_line, hist_title, hist_div_id = None):
+    trace0 = go.Histogram(
+        x = hist_vals,
+        nbinsx = 100,
+        text = nlog_null_qvals.index
+    )
 
-x = nlog_null_qvals[test_go_term]
+    data = [trace0]
+    
+    layout = {'title': hist_title, 
+        'yaxis': dict(title = 'Frequency'),
+        'xaxis': dict(title = '-log(Simulated q-value) vs. -log(observed q-value)'),
+        'hovermode': 'closest',
+        'bargap': 0.1, 
+        'shapes':[{'type': 'line',
+            'x0':hist_line, 
+            'y0':0,
+            'x1':hist_line,
+            'y1':1,
+            'yref': "paper",
+            'line':{'color':'red','width': 3}
+        }]
+    }
 
-trace0 = go.Histogram(
-    x = x,
-    nbinsx = 100,
-    text = nlog_null_qvals.index
-)
+    fig = dict(data=data, layout=layout)
 
-data = [trace0]
+    hist_div = plot(fig, validate = False, include_plotlyjs=False, output_type='div')
+    hist_div = hist_div.replace('\n', '')
+    
+    if hist_div_id: 
+        try:
+            existing_id = re.findall(r'id="(.*?)"|$', hist_div)[0]
+            hist_div = div.replace(existing_id, 'hist_div_id')
+        except IndexError:
+            pass
 
-layout = {'title': go_dict[test_go_term], 
-    'yaxis': dict(title = 'Frequency'),
-    'xaxis': dict(title = '-log(Simulated q-value) vs. -log(observed q-value)'),
-    'hovermode': 'closest',
-    'bargap': 0.1, 
-    'shapes':[{'type': 'line',
-        'x0':observed_df.loc[observed_df.GO_term == test_go_term].nlog_q_val.values[0], 
-        'y0':0,
-        'x1':observed_df.loc[observed_df.GO_term == test_go_term].nlog_q_val.values[0],
-        'y1':1,
-        'yref': "paper",
-        'line':{'color':'red','width': 3}
-    }]
-}
-
-fig = dict(data=data, layout=layout)
-
-hist_div = plot(fig, validate = False, include_plotlyjs=False, output_type='div')
+    return hist_div
 
 
 # In[8]:
 
 
-## Explore simulations with test statistics more extreme than observed statistic
-## in prep for plottin venn diagrams below
+# master dict to hold all data relvant to each GO term
+master_GO_dict = {}
 
-# go term of interest
-test_go_term = "GO:0086010" # user input goes here
+# list of GO names for dropdown menus later
+names = []
 
-# observed value
-test_stat = observed_df.loc[observed_df.GO_term == test_go_term].nlog_q_val.values[0]
+all_go_terms = scatter_df.GO_term
+#all_go_terms = ["GO:0086010"]
 
-# select simulation data which are more extreme
-outlier = nlog_null_qvals.loc[nlog_null_qvals[test_go_term] > test_stat].index.values
+for test_go_term in all_go_terms: 
+    # start dictionary with basic info
+    GO = {}
+    GO['go_id'] = test_go_term
+    GO['name'] = go_dict[test_go_term] # lookup from prior dict 
 
-# lookup in gene list dictionary
-outlier_samples = null_sample_dict[outlier]
+    # save list of names for dropdown menus later
+    names.append(go_dict[test_go_term])
+    
+    ## plotting histogram of null q-values
+    GO['hist_vals'] = list(nlog_null_qvals[test_go_term].values)
+    GO['hist_line'] = observed_df.loc[observed_df.GO_term == test_go_term].nlog_q_val.values[0]
+    
+    ## Explore simulations with test statistics more extreme than observed statistic
+    # observed value
+    test_stat = observed_df.loc[observed_df.GO_term == test_go_term].nlog_q_val.values[0]
 
-# lookup in sample dictionary
-outlier_genes = null_gene_dict.loc[null_gene_dict.index.isin(outlier)]
-outlier_genes = outlier_genes.loc[:, (outlier_genes != 0).any(axis = 0)]
-#outlier_genes = outlier_genes.transpose()
+    # select simulation data which are more extreme
+    outlier = nlog_null_qvals.loc[nlog_null_qvals[test_go_term] > test_stat].index.values
 
-# add on the observed data
-# samples
-outlier_samples['observed'] = observed_samples.observed.values
+    # lookup in gene list dictionary
+    outlier_samples = null_sample_dict[outlier]
 
-# genes
-outlier_genes = outlier_genes.transpose()
-outlier_genes['observed'] = 0
+    # lookup in sample dictionary
+    outlier_genes = null_gene_dict.loc[null_gene_dict.index.isin(outlier)]
+    outlier_genes = outlier_genes.loc[:, (outlier_genes != 0).any(axis = 0)]
 
-for i in outlier_genes.index.values:
-    if i in observed_genes.observed.values:
-        outlier_genes.at[i,'observed'] = 1
-    else:
-        continue
+    # add on the observed data
+    # samples
+    outlier_samples['observed'] = observed_samples.observed.values
 
-outlier_genes['Total_Observations'] = outlier_genes.sum(axis = 1)
-outlier_genes = outlier_genes[outlier_genes.columns[::-1]]
-#print(outlier_genes.head())
+    # genes
+    outlier_genes = outlier_genes.transpose()
+    outlier_genes['observed'] = 0
 
-# convert to list of lists (where each list is a row) for jquery
-gene_header = ['gene']
-gene_header.extend(outlier_genes.columns)
+    for i in outlier_genes.index.values:
+        if i in observed_genes.observed.values:
+            outlier_genes.at[i,'observed'] = 1
+        else:
+            continue
 
-#print(header)
-gene_header = [{"title":h} for h in gene_header]
+    outlier_genes['Total_Observations'] = outlier_genes.sum(axis = 1)
+    outlier_genes = outlier_genes[outlier_genes.columns[::-1]]
+    #print(outlier_genes.head())
 
-gene_table = []
-for i in range(len(outlier_genes.index)):
-    gene = outlier_genes.index.values[i]
-    tmp = [gene]
-    tmp.extend(outlier_genes.iloc[i,].values)
-    tmp = [str(x) for x in tmp]
-    gene_table.append(tmp)
+    # convert to list of lists (where each list is a row) for jquery
+    gene_header = ['gene']
+    gene_header.extend(outlier_genes.columns)
+
+    #print(header)
+    gene_header = [{"title":h} for h in gene_header]
+
+    # save to dict
+    GO['gene_header'] = gene_header
+    
+    gene_table = []
+    for i in range(len(outlier_genes.index)):
+        gene = outlier_genes.index.values[i]
+        tmp = [gene]
+        tmp.extend(outlier_genes.iloc[i,].values)
+        tmp = [str(x) for x in tmp]
+        gene_table.append(tmp)
+
+    # add to dictionary
+    GO['gene_table'] = gene_table
+
+    ## outlier samples
+    #print(outlier_samples.head())
+
+    df_arr = outlier_samples.values
+    groups = outlier_samples.columns.values
+    colnames = ['sample_id']
+    colnames.extend(groups)
+
+    empty_df = pd.DataFrame(columns=colnames)
+
+    for i, sample in enumerate(set(df_arr.flatten())):
+        tf_array = sample == df_arr
+        a = (np.array(np.where(tf_array == True)))
+        ii = a[-1]
+        vals = np.zeros(len(groups))
+        vals[ii] = 1
+        vals = list(vals)
+        col = [sample]
+        vals.insert(0, sample)
+        empty_df = empty_df.append(dict(zip(colnames, vals)), ignore_index = True)
+
+    sample_table = empty_df
+    sample_table['Total_Observations'] = sample_table.sum(axis = 1)
+
+    sample_table = sample_table.set_index(sample_table.sample_id)
+    sample_table.drop('sample_id', inplace = True, axis = 1)
+    sample_table = sample_table.astype(int)
+    sample_table = sample_table[sample_table.columns[::-1]]
+    #print(sample_table.head())
+
+    # convert to list of lists (where each list is a row) for jquery
+    sample_header = ['sample']
+    sample_header.extend(outlier_genes.columns)
+
+    #print(header)
+    sample_header = [{"title":h} for h in sample_header]
+
+    # save to dict
+    GO['sample_header'] = sample_header
+    
+    sample_lists = []
+    for i in range(len(outlier_samples.index)):
+        sample = sample_table.index.values[i]
+        tmp = [sample]
+        tmp.extend(sample_table.iloc[i,].values)
+        tmp = [str(x) for x in tmp]
+        sample_lists.append(tmp)
+
+    # add to dictionary
+    GO['sample_table'] = sample_lists
+    
+    # add all data from each GO term to dict, keyed by GO term ID
+    master_GO_dict[test_go_term] = GO
 
 
-# In[9]:
-
-
-## outlier samples
-#print(outlier_samples.head())
-
-df_arr = outlier_samples.values
-groups = outlier_samples.columns.values
-colnames = ['sample_id']
-colnames.extend(groups)
-
-empty_df = pd.DataFrame(columns=colnames)
-
-for i, sample in enumerate(set(df_arr.flatten())):
-    tf_array = sample == df_arr
-    a = (np.array(np.where(tf_array == True)))
-    ii = a[-1]
-    vals = np.zeros(len(groups))
-    vals[ii] = 1
-    vals = list(vals)
-    col = [sample]
-    vals.insert(0, sample)
-    empty_df = empty_df.append(dict(zip(colnames, vals)), ignore_index = True)
-
-sample_table = empty_df
-sample_table['Total_Observations'] = sample_table.sum(axis = 1)
-
-sample_table = sample_table.set_index(sample_table.sample_id)
-sample_table.drop('sample_id', inplace = True, axis = 1)
-sample_table = sample_table.astype(int)
-sample_table = sample_table[sample_table.columns[::-1]]
-#print(sample_table.head())
-
-# convert to list of lists (where each list is a row) for jquery
-sample_header = ['sample']
-sample_header.extend(outlier_genes.columns)
-
-#print(header)
-sample_header = [{"title":h} for h in sample_header]
-
-sample_lists = []
-for i in range(len(outlier_samples.index)):
-    sample = sample_table.index.values[i]
-    tmp = [sample]
-    tmp.extend(sample_table.iloc[i,].values)
-    tmp = [str(x) for x in tmp]
-    sample_lists.append(tmp)
-
-
-# In[10]:
+# In[27]:
 
 
 TEMPLATE = '''
 <!DOCTYPE html>
+
 <html>
     <head>
         <script src="https://cdn.plot.ly/plotly-latest.min.js"></script>
@@ -326,41 +365,51 @@ TEMPLATE = '''
         <script type="text/javascript" src="https://cdn.datatables.net/v/bs4/dt-1.10.18/sl-1.3.0/datatables.min.js"></script>
         <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.1/css/bootstrap.min.css">
         <style>body{ margin:0 100; background:white; }</style>
+        <meta charset="utf-8"/>
     </head>
     
     <body>
         <h1>First Quartile of PRS for EA in Probands</h1>
 
-        <!-- *** Section 1 *** --->
+        <!- - *** Section 1 *** - ->
         <h2>Simulated vs observed enrichment of GO terms</h2>
         [STICK_PLOT_DIV]
         <p>Red dots outside of blue bars are GO terms significant beyond proband ascertainment bias. Hover over \
         lines to get GO term ID, GO term name, observed -log q-value, and simulated mean and standard deviation.</p>
         
-        <!-- *** Section 2 *** --->
+        <!- - *** Section 2 *** - ->
         <h2>Fold enrichment as a function of p-value</h2>
         [PVAL_DIV]
         <p>Fold enrichment is calculated as the number of observed genes divided by the number of expected. \
         Hover over points to get GO term ID.</p>
 
-        <!-- *** Section 3 *** --->
+        <!- - *** Section 3 *** - ->
         <h2>Fold enrichment as a function total genes in GO term</h2>
         [SIZE_DIV]
         <p>Fold enrichment is calculated as the number of observed genes divided by the number of expected. \
         Hover over points to get GO term ID.</p>
 
-        <!-- *** Section 4 *** --->
+        <!- - *** Section 4 *** - ->
+        <h2> Analysis of Specific GO terms </h2>
+        <div>
+            <label>Select GO Term</label>
+            <select id="go_select">
+                [TERM_SELECT]
+            </select>
+        </div>
+        
         <h2>Histogram of GO terms that are more significant than expected by proband ascertainment bias</h2>
         [HIST_DIV]
         <p>Vertical red line indicates value of observed test statistic. Hover over bars to get simulation IDs from \
         Monte Carlo used to generate null hypothesis.</p>
 
-        <!-- *** Section 5 *** --->
+        <!- - *** Section 5 *** - ->
         <h2>Genes in simulated and observed groups</h2>
         <p>Cells indicate presence (1) or absence (0) of a gene in a group. Click colums to sort. \
         Genes present in all groups may represent a core set of genes driving GO enrichment \
         (Sort based on Total Observations). Control + click to sort based on second category. </p>
-        <table id="gene_table" class="table table-hover table-dark pb-3 display nowrap" width="100%"></table>
+        <table id="gene_table" class="table table-hover pb-3 display nowrap" width="100%"> </table>
+
         
         <h2>Samples in simulated and observed groups</h2>
         <p>Cells indicate presence (1) or absence (0) of a sample in a group. Click colums to sort. \
@@ -370,33 +419,59 @@ TEMPLATE = '''
     </body>
 
 <script>
-const gene_data = [GENE_DATA]
-const sample_data = [SAMPLE_DATA]
-const build_table = () => {
-    var gene_table = $("#gene_table").DataTable({
-        data: gene_data,
-        columns: [GENE_HEADER], 
+const data = [DATA]
+var gene_table
+var sample_table
+var go = Object.keys(data)[0]
+
+const build_hist = (go) => {
+    hist_vals = data[go]['hist_vals']
+    hist_line = data[go]['hist_line']
+    hist_name = data[go]['name']
+    
+    let hist_plot = document.getElementById("hist_div_id")
+    Plotly.react(hist_plot, hist_vals, hist_line, hist_name)
+}
+
+const build_table = (go) => {
+    if ( $.fn.DataTable.isDataTable('#gene_table') ) {
+      $('#gene_table').DataTable().destroy();
+    }
+
+    if ( $.fn.DataTable.isDataTable('#sample_table') ) {
+      $('#sample_table').DataTable().destroy();
+    }
+
+    gene_table = $("#gene_table").DataTable({
+        data: data[go]['gene_table'],
+        columns: data[go]['gene_header'], 
         // scrollY: '600px',
         scrollX: true,
         scrollCollapse: true,
         paging: true,
         pagingType: "simple",
-        info: true,
+        info: true
     })
-    var sample_table = $("#sample_table").DataTable({
-        data: sample_data,
-        columns: [SAMPLE_HEADER], 
+
+    sample_table = $("#sample_table").DataTable({
+        data: data[go]['sample_table'],
+        columns: data[go]['sample_header'], 
         // scrollY: '600px',
         scrollX: true,
         scrollCollapse: true,
         paging: true,
         pagingType: "simple",
-        info: true,
+        info: true
     })
 }
 
+$('#go_select').on("change", () => { 
+    go = $('#go_select :selected').val()
+    build_table(go)
+})
+
 $(document).ready(function() {
-    build_table()
+    build_table(go)
 })
 
 </script>
@@ -404,18 +479,24 @@ $(document).ready(function() {
 '''
 
 
-# In[11]:
+# In[28]:
 
 
-html = TEMPLATE.replace("[GENE_DATA]", str(gene_table))
-html = html.replace("[GENE_HEADER]", str(gene_header))
-html = html.replace("[SAMPLE_DATA]", str(sample_lists))
-html = html.replace("[SAMPLE_HEADER]", str(sample_header))
+# add in the data
+html = TEMPLATE.replace("[DATA]", json.dumps(master_GO_dict))
 
+# plots that only depend on the focal PRS, quartile, and proband/sibling status
 html = html.replace("[STICK_PLOT_DIV]", str(stick_plot_div))
 html = html.replace("[PVAL_DIV]", str(enrichment_by_pval_div))
 html = html.replace("[SIZE_DIV]", str(enrichment_by_size_div))
-html = html.replace("[HIST_DIV]", str(hist_div))
+html = html.replace("[HIST_DIV]", make_hist_div(master_GO_dict[list(master_GO_dict.keys())[0]]['hist_vals'], master_GO_dict[list(master_GO_dict.keys())[0]]['hist_line'], master_GO_dict[list(master_GO_dict.keys())[0]]['name']))
+
+select_term = []
+for key in name_dict.keys():
+    select_term.append("<option value = "+name_dict[key]+">"+key+"</option>")
+select_term = " ".join(select_term)
+
+html = html.replace("[TERM_SELECT]", select_term)
 
 f = open(report, "w")
 f.write(html)

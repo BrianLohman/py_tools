c12f7ac58c3adcc43d34d5e80c6dbdd8ed01af7e
BrianLohman <brian.keith.lohman@gmail.com>
2019-11-27 10:59:08 -0700

    more slurm args

--- batch_chpc.py
+++ batch_chpc.py
@@ -11,18 +11,20 @@ import re
 
 parser = argparse.ArgumentParser(description='generate and submit jobs to CHPC based on list of commands')
 parser.add_argument('-n','--job-name', dest='job', help = 'job base name')
-parser.add_argument('-c', '--commands', dest='commands', help = 'list of commands to run, one per line')
+parser.add_argument('-j', '--commands', dest='commands', help = 'list of commands to run, one per line')
 parser.add_argument('-w', '--ntasks-node', dest='ntasks', default = 8, help = 'number of tasks per node, default is 8 tasks')
 parser.add_argument('-m', '--mem', dest='mem', default = 32000, help = 'minimum memory required per node, default = 32GB')
 parser.add_argument('-p', '--partition', dest = 'partition', default = "hci-rw", help = 'hci-rw is only available choice')
 parser.add_argument('-t', '--time', dest='time', default = "12:00:00", type = str, help = 'max run time, default is 12 hours')
 parser.add_argument('-s', '--submit', dest = 'submit', default = True, type = bool, help = 'sbatch job files. or dont. Default is True.')
+parser.add_argument('-c', '--cpus_task', dest = 'cpus_task', default = 1, help = 'number of cpus required for each task')
 args = parser.parse_args()
 
 # count the number of lines in the commands file
 f = open(args.commands)
 contents = f.readlines()
 n_commands = 0
+
 for line in contents:
     stripped_line = line.strip()
     if re.match('^\s*#', stripped_line):
@@ -39,24 +41,24 @@ line_count = int(1)
 
 for line in open(args.commands):
     if job_fh == None:
-        job_fh = args.job + "_" + str(line_count)
-        o = open(job_fh + ".job", "w")
-        o.write('\n'.join(["#!/bin/bash", "#SBATCH --time="+args.time, \
-                "#SBATCH --account=hci-rw", "#SBATCH --partition="+args.partition, \
-                "#SBATCH --nodes=1", "#SBATCH --ntasks="+str(args.ntasks), \
-                "#SBATCH --mem="+str(args.mem), "#SBATCH --job-name="+job_fh, \
-                "#SBATCH -o call-"+job_fh+".out", "#SBATCH -e call-"+job_fh+".err", '\n', \
-                str(line)]))
+       job_fh = args.job + "_" + str(line_count)
+       o = open(job_fh + ".slurm", "w")
+       o.write('\n'.join(["#!/bin/bash", "#SBATCH --time="+args.time, \
+               "#SBATCH --account=hci-rw", "#SBATCH --partition="+args.partition, \
+               "#SBATCH --nodes=1", "#SBATCH --ntasks="+str(args.ntasks), "#SBATCH --cpus-per-task="+str(args.cpus_task), \
+               "#SBATCH --mem="+str(args.mem), "#SBATCH --job-name="+job_fh, \
+               "#SBATCH -o call-"+job_fh+".out", "#SBATCH -e call-"+job_fh+".err", '\n', \
+               str(line)]))
 
-    # if there is only one command to run
-        if line_count == n_commands:
-            o.close()
-            os.system("sbatch "+job_fh+".job")
-            break
+        # if there is only one command to run
+       if line_count == n_commands:
+           o.close()
+           os.system("sbatch "+job_fh+".slurm")
+           break
 
-        else:
-            line_count += 1
-            continue
+       else:
+           line_count += 1
+           continue
     
     else:
         if line_count == n_commands:
@@ -67,8 +69,8 @@ for line in open(args.commands):
             
             o.close()
             
-            if args.submit:
-                os.system("sbatch "+job_fh+".job")
+            if args.submit == True:
+                os.system("sbatch "+job_fh+".slurm")
             break
     
         if line_count % int(args.ntasks) != 0:
@@ -83,8 +85,9 @@ for line in open(args.commands):
         
         o.close()
         
-        if args.submit:
-            os.system("sbatch "+job_fh+".job")
+        if args.submit == True:
+            os.system("sbatch "+job_fh+".slurm")
+
         job_fh = None
         line_count += 1
         continue
